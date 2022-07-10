9bea62845a8cef58e30109a8dabd23b79ef518ed
Mohamed Kamal Abdelaziz Ahmed <mohamedkamal.info@gmail.com>
2022-07-10 22:41:00 +0200

    Added Section Active State

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ let navElement = document.querySelector('#inner-nav-bar');
 let docFragment = document.createDocumentFragment();
 let navClasses = document.querySelectorAll('.section-name');
 
-
-navClasses.forEach(function(navClass){  //Fill in nav bar with <li> elements
+//Fill in nav bar with <li> elements
+navClasses.forEach(function(navClass){
     let newLi = document.createElement("li");
     let newA = document.createElement("a");
     newA.textContent = navClass.firstElementChild.textContent;
@@ -20,5 +20,25 @@ navBarElem.forEach(function(elem,ind){
     navBarElem[ind].addEventListener('click',function(event){
         event.preventDefault();
         navClasses[ind].scrollIntoView({behavior:"smooth",block:"center"});
-    })
+    });
+});
+
+/*
+Set the active section.
+Determine which section is being viewed while scrolling through the page.
+*/
+navClasses.forEach(function(elm,ind){
+    document.addEventListener('scroll',function(){
+    let scrollPos = document.documentElement.scrollTop;
+        if(scrollPos >= navClasses[ind].offsetTop-navElement.parentElement.offsetHeight-150 && scrollPos <= navClasses[ind].offsetTop+navClasses[ind].offsetHeight)
+            {
+               navClasses.forEach(function(elem,i){
+                   navClasses[i].classList.remove("active");
+                   navBarElem[i].style.borderColor="white";
+               });
+                navClasses[ind].classList.add("active");
+                navBarElem[ind].style.borderColor="#00ace6";
+                navBarElem[ind].style.borderRadius="30px";
+            }
+    });
 });
