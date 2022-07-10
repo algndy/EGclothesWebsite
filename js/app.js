ce94b51730e1adb9fcfe979bac18b476724a6502
Mohamed Kamal Abdelaziz Ahmed <mohamedkamal.info@gmail.com>
2022-07-10 18:37:08 +0200

    Added scrollToView function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-//Fill in nav bar with <li> element
+//Global Variables
 let navElement = document.querySelector('#inner-nav-bar');
 let docFragment = document.createDocumentFragment();
 let navClasses = document.querySelectorAll('.section-name');
 
-navClasses.forEach(function(navClass){
+
+navClasses.forEach(function(navClass){  //Fill in nav bar with <li> elements
     let newLi = document.createElement("li");
     let newA = document.createElement("a");
-    newA.setAttribute("href",`#${navClass.id}`)
     newA.textContent = navClass.firstElementChild.textContent;
     newLi.appendChild(newA);
     docFragment.appendChild(newLi);
 });
-
 navElement.appendChild(docFragment);
+
+
+//Clicking an item from the navigation menu, the link scroll to the appropriate section.
+let navBarElem = document.querySelectorAll('#inner-nav-bar li a');
+navBarElem.forEach(function(elem,ind){
+    navBarElem[ind].addEventListener('click',function(event){
+        event.preventDefault();
+        navClasses[ind].scrollIntoView({behavior:"smooth",block:"center"});
+    })
+});
