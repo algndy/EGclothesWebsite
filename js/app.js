5cdb8b543a0b49b09662d124e63ae3a385e31f80
Mohamed Kamal Abdelaziz Ahmed <mohamedkamal.info@gmail.com>
2022-07-11 21:57:33 +0200

    modified logo and category section

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ navBarElem.forEach(function(elem,ind){
 Set the active section.
 Determine which section is being viewed while scrolling through the page.
 */
+navBarElem[0].style.cssText+="border:solid 1px #00ace6;border-radius:30px";
 navClasses.forEach(function(elm,ind){
     document.addEventListener('scroll',function(){
     let scrollPos = document.documentElement.scrollTop;
@@ -43,3 +44,32 @@ navClasses.forEach(function(elm,ind){
             }
     });
 });
+
+
+//Make the logo on the video appears with special style
+let logoOnVideo = document.querySelector('.text-on-video .logo-on-video');
+setTimeout(function(){                   
+    logoOnVideo.style.cssText=`position:relative;opacity:0;top:30px;`;
+    setTimeout(function(){
+        logoOnVideo.style.cssText+=`transition:top 1s,opacity 2s;opacity:1;top:0;`;
+    },300);
+},0);
+
+
+
+//function for move element from outer page right to inner page left
+let categoryDiv = document.querySelector('#categories');
+function moveItemFromRightToLeft(elem,duration)
+{
+    elem.style.cssText = `position:relative; left:${elem.offsetWidth}px;transition: left ${duration}s;`;
+    window.addEventListener("scroll",function(){
+        if(window.scrollY >= elem.offsetTop-550)
+        {
+            //console.log(`${window.scrollY} , ${elem.offsetTop} `)
+            elem.style.left="0px";
+        }
+    });
+}
+
+moveItemFromRightToLeft(categoryDiv,1);
+
